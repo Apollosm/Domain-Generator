2bf3401b62ee7974531693afa0061fa297b86d41
Luis Camargo <luismiguelcamar@gmail.com>
2022-11-07 04:50:37 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "my", "our", "your"];
+  let adj = ["big", "lust", "kind", "amazing", "wholesome", "furious"];
+  let noun = ["journey", "tips", "home", "space", "room", "diary"];
+  let extensiones = [".com", ".net", ".es", ".io", ".dev"];
+
+  for (let a of pronoun) {
+    for (let b of adj) {
+      for (let c of noun) {
+        for (let d of extensiones) {
+          console.log(`${a}${b}${c}${d}`);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
